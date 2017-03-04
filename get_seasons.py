3515290e424d48e9d7e80106fa02188f422fc8ef
kylerayner8 <kylerayner8@gmail.com>
2017-03-04 01:03:01 -0600

    Season ids
    Adding script to try to filter which seasons we played.
    Will probably need to look for team names rather than league name.

---
+++ get_seasons.py
@@ -0,0 +1,34 @@
+import requests
+import template
+from bs4 import BeautifulSoup
+import os
+
+
+def get_season_data():
+    cur_season = template.current_season_index
+    end_index = cur_season - 1
+    results = open("results.txt", 'w')
+
+    while cur_season > end_index:
+        url = template.base_season_url.format(cur_season)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, "lxml")
+
+        headings = soup.findAll('span', class_="pagLeagueHeading")
+        we_played = False
+        for heading in headings:
+            text_field = heading.text.lower()
+            if 'sunday' in text_field and 'rec' in text_field:
+                we_played = True
+
+        if we_played is True:
+            results.write(str(cur_season))
+            results.write('\n')
+        cur_season = cur_season - 1
+
+    results.close()
+    return None
+
+if 'results.txt' in os.listdir():
+    os.remove('results.txt')
+get_season_data()
--- template.py
+++ template.py
@@ -1,5 +1,7 @@
 old_season = "http://www.myteamsports.net/prd/league/myleague.asp?orgid=5&orgsportid=32&leagueID=770&seasonID=8639"
 url = "http://www.myteamsports.net/prd/league/myleague.asp?orgid=5&orgsportid=32&leagueID=770&seasonID=8692"
+base_season_url = "http://www.myteamsports.net/prd/league/myleague.asp?orgid=5&orgsportid=32&leagueID=770&seasonID={0}"
+current_season_index = 8692
 email = "{0} vs. {1}\nGame Time: {2}\nLocation: {3}"
 
 
